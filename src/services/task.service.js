259e97bd4ad8efe56fcbd4ad19e2b2d89e10f05e
Safa <sayyujahan@gmail.com>
2026-04-05 09:34:17 +0530

    Updated project

--- src/services/task.service.js
+++ src/services/task.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { ITaskService } from "./interfaces/ITaskService";
 
-const API_URL = import.meta.env.VITE_API_URL ||"http://localhost:5000/api/tasks";
+const API_URL = `${import.meta.env.VITE_API_URL}/api/tasks` || "http://localhost:5000/api/tasks";
 
 export class TaskService extends ITaskService {
   async getTasks(status) {
